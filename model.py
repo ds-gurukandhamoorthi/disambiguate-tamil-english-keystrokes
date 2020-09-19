c043fac24c98831bc27cfc71dbd56464f8501c8f
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2020-09-19 06:16:50 +0200

    Modelize with linear models, Keep same proportions as the original dataset

--- model.py
+++ model.py
@@ -3,6 +3,10 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import make_pipeline
 from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import RidgeClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import ShuffleSplit
 
 if __name__ == "__main__":
     word_stat = pd.read_csv('data/tamil-word-stats.csv')
@@ -26,9 +30,12 @@ if __name__ == "__main__":
     word_stat['keystrokes'] = word_stat['inscript_keystrokes']
     word_stat = word_stat[['word', 'count', 'keystrokes','lang']]
 
-    # both = pd.concat([word_stat, eng_word_stat])
+    both_full = pd.concat([word_stat, eng_word_stat])
 
-    both = pd.concat([word_stat[:20000], eng_word_stat[:20000]])
+    rs = ShuffleSplit(n_splits=1, test_size=.02, random_state=0)
+    _, smaller_indices = next(rs.split(both_full))
+    both = both_full.iloc[smaller_indices]
+    # both = pd.concat([word_stat[:20000], eng_word_stat[:20000]])
 
     both = (both
         .dropna(subset=['keystrokes']))
@@ -47,6 +54,27 @@ if __name__ == "__main__":
     pipe = make_pipeline(
             CountVectorizer(analyzer='char', ngram_range=(2,2), preprocessor=lambda s: f' {s} '),
             SVC(C=100)
+            )
+    pipe.fit(keystrokes, labels)
+    print(f'Score on training set: {pipe.score(keystrokes,labels)}')
+
+    pipe = make_pipeline(
+            CountVectorizer(analyzer='char', ngram_range=(1,2), preprocessor=lambda s: f' {s} '),
+            LogisticRegression()
+            )
+    pipe.fit(keystrokes, labels)
+    print(f'Score on training set: {pipe.score(keystrokes,labels)}')
+
+    pipe = make_pipeline(
+            CountVectorizer(analyzer='char', ngram_range=(1,2), preprocessor=lambda s: f' {s} '),
+            RidgeClassifier(alpha=0.1)
+            )
+    pipe.fit(keystrokes, labels)
+    print(f'Score on training set: {pipe.score(keystrokes,labels)}')
+
+    pipe = make_pipeline(
+            CountVectorizer(analyzer='char', ngram_range=(1,2), preprocessor=lambda s: f' {s} '),
+            SGDClassifier(penalty='elasticnet', loss='log', alpha=0.000001)
             )
     pipe.fit(keystrokes, labels)
     print(f'Score on training set: {pipe.score(keystrokes,labels)}')
